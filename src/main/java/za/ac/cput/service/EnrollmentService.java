5f35aac6c2fcad4dad257b6b21db56815972eb34
Uwaiz Laher <129673853+CaffeinatedPanda15@users.noreply.github.com>
2025-03-26 21:26:38 +0200

    First flight

---
+++ src/main/java/za/ac/cput/service/EnrollmentService.java
@@ -0,0 +1,22 @@
+package za.ac.cput.service;
+
+import java.util.Set;
+
+public class EnrollmentService {
+    private final EnrollmentRepository enrollmentRepository = new EnrollmentRepository();
+
+    public Enrollment enrollStudent(Enrollment enrollment) {
+
+        return enrollmentRepository.create(enrollment);
+    }
+
+    public Enrollment getEnrollmentByStudentId(String studentId) {
+
+        return enrollmentRepository.read(studentId);
+    }
+
+    public Set<Enrollment> getAllEnrollments() {
+
+        return enrollmentRepository.getAll();
+    }
+}
